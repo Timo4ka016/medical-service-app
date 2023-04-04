56d2fec809109d8d0f2c6cc0e0adb3748db0acff
Timo4ka <lib_terror@mail.ru>
2023-04-04 23:57:40 +0600

    backend

---
+++ med-app-backend-spring/src/main/java/com/medapp/app/dts/medappbackendspring/MedAppBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.medapp.app.dts.medappbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAppBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedAppBackendSpringApplication.class, args);
+	}
+
+}
